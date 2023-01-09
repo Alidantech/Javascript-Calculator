aa8cac5f7deec658a1988041afe3d7537339eabb
PETER IRUNGU <peteralidante254@gmail.com>
2023-01-09 20:46:37 +0300

    two classes

---
+++ Main.java
@@ -0,0 +1,11 @@
+import javax.swing.JOptionPane;
+public class Main{
+     public static void main(String[] args) {
+        System.out.println("Hello world!");
+        int x = 4;
+        int y = 5;
+        int sum = x + y;
+        System.out.println(sum);
+       JOptionPane.showMessageDialog(null,"hello world");
+    }
+}
