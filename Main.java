300ebaa70371e1de78c79cd73e44464ecb543061
PETER IRUNGU <peteralidante254@gmail.com>
2023-01-09 21:22:39 +0300

    javaWithUI

--- Main.java
+++ Main.java
@@ -1,11 +1,16 @@
+import java.awt.Color;
+import javax.swing.JFrame;
 import javax.swing.JOptionPane;
+import javax.swing.JButton;
 public class Main{
      public static void main(String[] args) {
-        System.out.println("Hello world!");
-        int x = 4;
-        int y = 5;
-        int sum = x + y;
-        System.out.println(sum);
-       JOptionPane.showMessageDialog(null,"hello world");
+        JFrame mainFrame = new JFrame();
+        mainFrame.setSize(500, 400);
+        mainFrame.setBackground(Color.red);
+        mainFrame.setVisible(true);
+        JButton press = new JButton();
+        press.setBounds(10, 20,10,30);
+        mainFrame.add(press);
+        JOptionPane.showMessageDialog(mainFrame,"hello world");
     }
 }
