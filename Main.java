4f66cddc32d73dd6b7b511a5e200847caa474d47
PETER IRUNGU <peteralidante254@gmail.com>
2023-01-16 21:31:47 +0300

    two classes

--- Main.java
+++ Main.java
@@ -1,16 +1,15 @@
-import java.awt.Color;
+import java.awt.*;
 import javax.swing.JFrame;
-import javax.swing.JOptionPane;
-import javax.swing.JButton;
+import javax.swing.JTextBox;
 public class Main{
      public static void main(String[] args) {
         JFrame mainFrame = new JFrame();
         mainFrame.setSize(500, 400);
         mainFrame.setBackground(Color.red);
         mainFrame.setVisible(true);
-        JButton press = new JButton();
-        press.setBounds(10, 20,10,30);
-        mainFrame.add(press);
-        JOptionPane.showMessageDialog(mainFrame,"hello world");
+        JTextBox text = new JTextBox();
+        String word = text();
+        UpperCheck uc = new UpperCheck();
+        uc.display(word);
     }
 }
---
+++ UpperCheck.java
@@ -0,0 +1,7 @@
+import javax.swing.JOptionPane;
+public class UpperCheck{
+  public String display(String word){
+    if
+    JOptionPane.showMessageDialog(mainnFrame, "You have written the word in wrong format.");
+     }
+}
