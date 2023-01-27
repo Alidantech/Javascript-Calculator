22b006ee9a324c5186522592f5c71770e2e45128
PETER IRUNGU <peteralidante254@gmail.com>
2023-01-28 01:03:58 +0300

    music

---
+++ Music App/main.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+import java.awt.*;
+class main{
+        public static void main(String args[]){
+            JFrame frame = new JFrame();
+            frame.setBackground(Color.GREEN);
+            frame.setVisible(true);
+            frame.setSize(400, 700);
+    }
+}
