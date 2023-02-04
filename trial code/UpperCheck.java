354c356f4a558f245b7359691247aede13e98ffe
PETER IRUNGU <peteralidante254@gmail.com>
2023-02-04 19:58:00 +0300

    Java with xml layout

--- trial code/UpperCheck.java
+++
@@ -1,7 +0,0 @@
-import javax.swing.JOptionPane;
-public class UpperCheck{
-  public String display(String word){
-    if
-    JOptionPane.showMessageDialog(mainnFrame, "You have written the word in wrong format.");
-     }
-}
